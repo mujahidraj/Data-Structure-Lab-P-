f9ccb74892bdb3a504c5d97a6bc334d76f0c61fa
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2022-09-27 13:12:11 +0600

    Add files via upload

---
+++ lab task 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+    int main () {
+
+
+    int arr[5]={10,20,30,40,50};
+    int arr2[8]={1,2,3,4,5,6,7,8};
+
+    for (int j=7;j>=0;j--){
+
+
+       cout<<" "<<arr2[j];
+    }
+
+    for (int i=4;i>=0;i--){
+
+
+       cout<<" "<<arr[i];
+    }
+
+
+
+    return 0;
+    }
